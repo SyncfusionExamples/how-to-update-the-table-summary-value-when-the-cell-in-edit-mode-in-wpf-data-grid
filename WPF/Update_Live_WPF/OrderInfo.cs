5ebe200c0a4497f20f259777afb589d9b8ac8266
Dhanasekar Mohanraj <dhanasekar.mohanraj@syncfusion.com>
2020-11-03 16:15:31 +0530

    How to update the table summary value when the cell in edit mode in WPF DataGrid (SfDataGrid) ?

---
+++ WPF/Update_Live_WPF/OrderInfo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Update_Live_WPF
+{
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        int orderID;
+        string customerId;
+        string country;
+        string customerName;
+        int unitPrice;
+
+        public int OrderID
+        {
+            get { return orderID; }
+            set { orderID = value; }
+        }
+
+        public string CustomerID
+        {
+            get { return customerId; }
+            set { customerId = value; }
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public int UnitPrice
+        {
+            get
+            {
+                return unitPrice;
+            }
+            set
+            {
+                unitPrice = value;
+                OnPropertyChanged("UnitPrice");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public OrderInfo(int orderId, string customerName, string country, string customerId, int unitPrice)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.UnitPrice = unitPrice;
+        }
+    }
+}
